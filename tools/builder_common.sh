d55c958dd800788fb6dfaedbd3675afd98edaebf
Jonatan Ramos Bonilla <jntramosbonilla@protonmail.com>
2017-09-06 17:47:56 +0100

    Fix pfSense-upgrade module rename script

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -2220,7 +2220,7 @@ poudriere_rename_ports() {
 
 		# pfSense-upgrade files
 		if [ -d ${_pdir}/${_pname}/files ]; then
-			for fd in $(find ${_pdir}/${_pname}/files -type f -name '*pfSense*'); do
+			for fd in $(find ${_pdir}/${PRODUCT_NAME}-upgrade/files -type f -name '*pfSense*'); do
 				local _fddir=$(dirname ${fd})
 				local _fdname=$(echo $(basename ${fd}) | sed "s,pfSense,${PRODUCT_NAME},")
 
